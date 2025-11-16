9c6dccb39fc465edfe91ca8c01142d15818478d1
Fellipe Rocha <felliperocha464@gmail.com>
2025-11-16 16:32:03 -0300

    realizando login

--- helpdesk/obj/Debug/net9.0-android/helpdesk.AssemblyInfo.cs
+++ helpdesk/obj/Debug/net9.0-android/helpdesk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helpdesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c1e7d7486d1ebc5679826471ced028fc48c3230c")]
 [assembly: System.Reflection.AssemblyProductAttribute("helpdesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helpdesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
